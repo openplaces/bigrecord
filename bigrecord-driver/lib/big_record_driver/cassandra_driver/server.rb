f745ef8230fc63c66ca359faedf0c9f774684a9a
Jean-Daniel Cryans <jdcryans@apache.org>
2009-07-14 09:41:17 -0400

    Adding some more get and get_columns

--- bigrecord-driver/lib/big_record_driver/cassandra_driver/server.rb
+++ bigrecord-driver/lib/big_record_driver/cassandra_driver/server.rb
@@ -20,13 +20,13 @@ class CassandraServer < BigRecordServer
     @config = config
     init_connection
   end
-
+  
   def update(table_name, row, values, timestamp=nil)
     safe_exec do
       return nil unless row
       timestamp = 0 unless timestamp
-      values.each do |column, value|
-        @cassandraClient.insert(table_name.to_s, row, column, value.to_bytes, timestamp, true)
+      values.each do |column, value|  
+      @cassandraClient.insert(table_name.to_s, row, column, value.to_bytes, timestamp, true)
       end
       row
     end
@@ -35,8 +35,62 @@ class CassandraServer < BigRecordServer
   def get(table_name, row, column, options={})
     safe_exec do
       return nil unless row
-      column = @cassandraClient.get_column(table_name.to_s, row, column)
-      Java::String.new(column.value).to_s
+      # Retreive only the last version by default
+      options[:num_versions] ||= 1
+
+      # validate the arguments
+      raise ArgumentError, "num_versions must be >= 1" unless options[:num_versions] >= 1
+      begin
+        if options[:timestamp]
+          raw_data = @cassandraClient.get_columns_since(table_name.to_s, row, column, options[:timestamp])
+        else
+          raw_data = @cassandraClient.get_column(table_name.to_s, row, column)
+        end
+      rescue NotFoundException => e2
+        puts e2.message
+        puts e2.class
+      end
+      # Return either a single value or an array, depending on the number of version that have been requested
+      if options[:timestamp]
+        return [] unless raw_data
+        max_index = raw_data.length > options[:num_versions] || raw_data.length
+        0..max_index.each do |i|
+          arr[i] = Java::String.new(raw_data[i].value).to_s
+        end
+        arr
+      else
+        return nil unless raw_data
+        Java::String.new(raw_data.value).to_s
+      end
+    end
+  end
+
+  def get_columns(table_name, row, columns, options={})
+    safe_exec do
+      return nil unless row
+      raise ArgumentError, "timestamp on get_columns is not currently supported with cassandra" if options[:timestamp]
+      arr = []
+      columns.each_with_index do |col, i|
+        begin
+          if col[-1,1] == ':'
+            arr + @cassandraClient.get_slice(table_name.to_s, row, col, -1, -1).to_a
+          else
+            arr + @cassandraClient.get_column(table_name.to_s, row, col)
+          end
+        rescue NotFoundException => e2
+          puts e2.message
+          puts e2.class
+        end
+      end
+      unless !result or result.isEmpty
+        values = {}
+        arr.each do |column_t|
+          values[column_t.getColumnName.to_s] = Java::String.new(column_t.value).to_s
+        end
+        values["attribute:id"] = row
+        values
+      end
+      
     end
   end
 
--- bigrecord-driver/test/test_client_cassandra.rb
+++ bigrecord-driver/test/test_client_cassandra.rb
@@ -13,7 +13,10 @@ class TestCassandraClient < Test::Unit::TestCase
       @big_db = BigRecordDriver::Client.new(:drb_port => 40005)
     end
   end
+  
+ def test_update_without_timestamps
 
+  end
 
   def test_update_with_timestamps_in_chronological_order
   end
@@ -22,6 +25,31 @@ class TestCassandraClient < Test::Unit::TestCase
   end
 
   def test_get_and_get_columns
+    t1 = Time.now.to_i
+    t2 = t1 + 1000
+    t3 = t2 + 1000
+    # Temporary copy-paste until all tests passes
+    @big_db.update(TABLE_NAME,
+                    'dog-key',
+                   {'columnfamily1:name' => 'Dog',
+                    'columnfamily1:size' => 'medium',
+                    'columnfamily2:toto' => 'some value1'},
+                    t1)
+
+    @big_db.update(TABLE_NAME,
+                    'dog-key',
+                   {'columnfamily1:size' => 'small',
+                    'columnfamily2:toto' => 'some value2'},
+                    t2)
+
+    @big_db.update(TABLE_NAME,
+                    'dog-key',
+                   {'columnfamily1:size' => 'big'},
+                    t3)
+
+    # normal calls
+    assert_equal 'big', @big_db.get(TABLE_NAME, 'dog-key', 'columnfamily1:size'), "Didn't retrieved the last version of the cell"
+    assert_nil @big_db.get(TABLE_NAME, 'dog-key-that-does-not-exist', 'columnfamily1:size'), "Got a value for a cell that doesn't even exist"
   end
 
   def test_get_consecutive_rows

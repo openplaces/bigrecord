ee32e79298bc786066cee3cce51d06e2b6ee3e1c
Greg Lu <greg.lu@gmail.com>
2009-08-25 13:43:27 -0400

    Bigindex update
    * Really silly mistake: used a single quote instead of double.

--- bigindex/init.rb
+++ bigindex/init.rb
@@ -1,3 +1,3 @@
 require 'big_index'
 
-BigIndex.configurations = YAML::load(File.open("#{RAILS_ROOT}/config/bigindex.yml'))
+BigIndex.configurations = YAML::load(File.open("#{RAILS_ROOT}/config/bigindex.yml"))

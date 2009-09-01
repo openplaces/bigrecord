09047851da0dd00269bad96e0b5eea26c25448d3
Jean-Daniel <jdcryans@jdcryans-laptop.(none)>
2009-09-01 14:42:53 -0400

    Upgrading to hbase-0.20.0

--- bigrecord-driver/lib/big_record_driver/column_descriptor.rb
+++ bigrecord-driver/lib/big_record_driver/column_descriptor.rb
@@ -4,7 +4,6 @@ module BigRecordDriver
 
     attr_accessor :name
     attr_accessor :versions
-    attr_accessor :max_value_length
     attr_accessor :in_memory
     attr_accessor :bloom_filter
     attr_accessor :compression
@@ -14,7 +13,6 @@ module BigRecordDriver
 
       @name = name.to_s
       @versions         = options[:versions]
-      @max_value_length = options[:max_value_length]
       @in_memory        = options[:in_memory]
       @bloom_filter     = options[:bloom_filter]
       @compression      = options[:compression]

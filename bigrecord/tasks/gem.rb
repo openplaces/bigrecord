5cb9860c74d9358a94832bc49b0ab8c695016f4d
Greg Lu <greg.lu@gmail.com>
2009-10-12 02:08:22 -0400

    Jeweler and gemspec updated.

--- bigrecord/tasks/gem.rb
+++ bigrecord/tasks/gem.rb
@@ -8,10 +8,15 @@ begin
     gemspec.homepage = "http://www.bigrecord.org"
     gemspec.summary = "Object mapper for supporting column-oriented data stores (supports #{DATA_STORES.join(" ")}) in Ruby on Rails."
     gemspec.description = "BigRecord is built from ActiveRecord, and intended to seamlessly integrate into your Ruby on Rails applications."
+    gemspec.files = FileList["{doc,examples,generators,lib,spec,tasks}/**/*","init.rb","install.rb","Rakefile","VERSION"].to_a
+    gemspec.extra_rdoc_files = FileList["doc/**/*","MIT-LICENSE","README.rdoc"].to_a
 
+    gemspec.add_development_dependency "rspec"
     gemspec.add_dependency "uuidtools", ">= 2.0.0"
     gemspec.add_dependency "bigrecord-driver"
   end
+  Jeweler::GemcutterTasks.new
 rescue LoadError
   puts "Jeweler not available. Install it with: sudo gem install technicalpickles-jeweler -s http://gems.github.com"
 end
+

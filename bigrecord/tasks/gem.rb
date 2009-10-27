2017d5a70ac5d06f1b6b020d49c5912f8ae7c857
Greg Lu <greg.lu@gmail.com>
2009-10-27 17:27:26 -0400

    Modified the jeweler task.

--- bigrecord/tasks/gem.rb
+++ bigrecord/tasks/gem.rb
@@ -9,7 +9,7 @@ begin
     gemspec.summary = "Object mapper for supporting column-oriented data stores (supports #{DATA_STORES.join(" ")}) in Ruby on Rails."
     gemspec.description = "BigRecord is built from ActiveRecord, and intended to seamlessly integrate into your Ruby on Rails applications."
     gemspec.files = FileList["{doc,examples,generators,lib,rails,spec,tasks}/**/*","init.rb","install.rb","Rakefile","VERSION"].to_a
-    gemspec.extra_rdoc_files = FileList["doc/**/*","MIT-LICENSE","README.rdoc"].to_a
+    gemspec.extra_rdoc_files = FileList["doc/**/*","LICENSE","README.rdoc"].to_a
 
     gemspec.add_development_dependency "rspec"
     gemspec.add_dependency "uuidtools", ">= 2.0.0"

d790d65c081402c0b66a810edc56d5bcf2f23333
Greg Lu <greg.lu@gmail.com>
2010-01-14 14:58:42 -0500

    Aliasing the old BigRecordDriver namespace to BigRecord::Driver

--- bigrecord-driver/lib/big_record_driver.rb
+++ bigrecord-driver/lib/big_record_driver.rb
@@ -4,3 +4,11 @@ require BRD_ROOT + '/big_record_driver/client'
 require BRD_ROOT + '/big_record_driver/exceptions'
 require BRD_ROOT + '/big_record_driver/column_descriptor'
 require BRD_ROOT + '/big_record_driver/version'
+
+module BigRecord
+  module Driver
+  end
+end
+
+# Aliasing the old namespace
+BigRecordDriver = BigRecord::Driver
